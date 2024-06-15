b205f2416d1a830ab32083764ad44d256c36bb23
shivam mole <126333403+shivammole1234@users.noreply.github.com>
2024-06-15 11:40:21 +0000

    first commit

---
+++ datatype.js
@@ -0,0 +1,4 @@
+console.log("hii how asre yiou ")
+
+console.log(typeof null); // in JS nulll is object so the type of null is object 
+console.log(typeof undefined); // output will be undefined 
---
+++ script.js
@@ -0,0 +1,6 @@
+const acntId ='shivam12';
+const actNam='shivam';
+const city = 'pune';
+let acntState;
+
+console.table([acntId,actNam,city,acntState]);
